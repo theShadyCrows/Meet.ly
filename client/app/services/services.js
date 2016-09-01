d5577043a8632315bdf3ed0a27939b2d3ecaf8ed
H3mann <mann.harp@gmail.com>
2016-09-01 11:02:38 -0400

    added citibike factory to services

--- client/app/services/services.js
+++ client/app/services/services.js
@@ -84,6 +84,26 @@ angular.module('MeetlyApp.services', [])
 });
 
 
+//citiBike factory
+
+.factory ('citibikeFactory', function ($http) {
+
+  var getData = function () {
+
+    return $http({
+      method: 'GET',
+      url: 'http://api.citybik.es/citi-bike-nyc.json'
+    })    
+  };
+
+  return {
+    getData: getData
+  };
+  console.log('%%%',getData);
+
+});
+
+
 
 
 // params (in the URL) is for GET requests
